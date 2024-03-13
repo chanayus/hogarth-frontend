c3fc1032d20e7f10d8b47f50382437aecc54b434
Xhanayus <chanayus1526@gmail.com>
2024-03-13 21:15:06 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,27 @@
 const mobileNavToggleButton = document.querySelector("#mobile-nav-button");
 const mobileNav = document.querySelector("#mobile-nav");
+const mobileNavContent = document.querySelector(".mobile-nav-content");
 
-//   เปิด/ปิด mobile navigation menu
+//  เปิด/ปิด mobile navigation menu
 mobileNavToggleButton?.addEventListener("click", () => {
   const isNavHidden = mobileNav.classList.contains("hidden");
 
-  //   เช็กว่า mobile navigation menu ปิดอยู่หรือไม่
+  //  เช็คว่า mobile navigation menu ปิดอยู่หรือไม่
   if (isNavHidden) {
-    mobileNav.classList.remove("hidden");
+    mobileNav.classList.remove("hidden"); // แสดง mobile navigation menu
+
+    // ทำการซ่อน mobile navigation menu เมื่อมีการคลิกส่วนที่ไม่ใช่เมนู mobile navigation
+    mobileNav.addEventListener(
+      "click",
+      (e) => {
+        e.stopPropagation();
+        if (!mobileNavContent.contains(e.target)) {
+          mobileNav.classList.add("hidden");
+        }
+      },
+      { capture: true, once: true }
+    );
   } else {
-    mobileNav.classList.add("hidden");
+    mobileNav.classList.add("hidden"); // ซ่อน mobile navigation menu
   }
 });
